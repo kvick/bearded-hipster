2c33599b9b995ea8d6c0629e81ee8dea275df0ce
kvick <kvick@netflix.com>
2013-08-09 00:50:21 -0700

    processes each lib, can't compare tho:

--- how_far.py
+++ how_far.py
@@ -17,34 +17,25 @@ import requests
 def versiontuple(v):
     return tuple(map(int, (v.split("."))))
 
-def parse_file2(log, filename):
+def get_for_lib(log, org, lib, version):
+    """docstring for get_for_lib"""
+
+    module_url = '{0}/{1}'.format(org, lib)
+    log.info('processing {0} for version {1}'.format(module_url, version))
+    versions = get_versions_from_scruffy(log, module_url)
+    compare_versions(log, versions, version)
+
+def parse_file(log, filename):
     """"""
     with open(filename, 'r') as dep_file:
        for line in dep_file:
            if line:
                match = re.search('^([\w\d.]+)#([\w\d-]+);([\d.-]+)', line)
                if match:
-                   print 'org={0}, lib={1}, version={2}'.format(match.group(1),
+                   log.info('org={0}, lib={1}, version={2}'.format(match.group(1),
                                                                 match.group(2),
-                                                                match.group(3))
-
-def parse_file(log, filename):
-    """ tried to speed things up by using a compiled pattern, doesn't work :( """
-    with open(filename, 'r') as dep_file:
-       regex = re.compile("(?P<org>^([\w\d.]+)*?)#(?P<lib>([\w\d-]+)*?);(?P<version>([\d.-]+))$")
-       for line in dep_file:
-           if line:
-               stripped_line = line.strip()
-               print 'processing line = [{0}]'.format(stripped_line)
-               matches = regex.search(stripped_line)
-
-               if matches:
-                   results = matches.groupdict()
-                   print 'org={0}, lib={1}, version={2}'.format(results['org'],
-                                                                results['lib'],
-                                                                results['version'])
-               else:
-                   log.debug('no match found in [{0}]'.format(line))
+                                                                match.group(3)))
+                   get_for_lib(log, match.group(1),match.group(2),match.group(3))
 
 def get_versions_from_scruffy(log, module_url):
     """docstring for get_versions_from_scruffy"""
@@ -54,10 +45,35 @@ def get_versions_from_scruffy(log, module_url):
     soup = BeautifulSoup(r.text)
     
     release_versions = []
+    # find by the CSS 'rev release' class
     for revision in soup.find_all("a", class_="rev release"):
-        release_versions.append(revision.contents)
+        release_versions.append(str(revision.contents))
 
-    print release_versions
+    log.debug(" =========== release versions ========== ") 
+    log.debug(release_versions)
+    return release_versions
+
+def compare_versions(log, module_versions, version):
+    """given a list of ordered versions if the version is in this list
+       print how far away the given version is from latest (last) element"""
+    
+    log.debug("looking for %s" % version)
+
+    length = len(module_versions)
+    if not version in module_versions:
+        log.info('didn\'t find {0} in {1} versions'.format(version, length))
+        log.debug(module_versions)
+        return None
+    else:
+        index = module_versions.index(version)
+        log.debug('{0} found at position {1} of {2}'.format(version, index + 1, length))
+        newer_versions = module_versions[index - length:]
+        newer_versions.remove(version)
+        if len(newer_versions) > 0:
+            log.debug('newer versions = {0}'.format(newer_versions)) 
+        newer_count = len(newer_versions)
+        log.info('there are {0} newer version(s) available'.format(newer_count))
+        return newer_versions
 
 def main():
     logging.basicConfig(filename='dep.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -71,10 +87,15 @@ def main():
     p.add_option('--file', '-f', default="transitive-dependencies.txt")
     options, arguments = p.parse_args()
 
-    parse_file2(log, options.file)
+    parse_file(log, options.file)
+    #release_versions = get_versions_from_scruffy(log, 'netflix/astyanax')
+    #compare_versions(log, release_versions, '1.34.0')
 
-    get_versions_from_scruffy(log, 'netflix/astyanax')
-    log.info("finished")
+    #compare_versions(log, [u'1.34.0'], '1.34.0')
+    #compare_versions(log, [u'1.33.0', u'1.34.0', u'1.35.0'], '1.34.0')
+    #compare_versions(log, [u'1.34.0', u'1.35.0'], '1.39.0')
+
+    log.info("Finished")
 
 if __name__ == '__main__':
     main()
