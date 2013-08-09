f4ac9ae574a10813d7de97e070e1cabbfae71734
kvick <kvick@netflix.com>
2013-08-08 23:46:22 -0700

    first scrape of scruffy

--- how_far.py
+++ how_far.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Step 1. For Netflix Detect that I got a version older
+# Step 1. For Netflix libs Detect that I got a version older
 # parse transitive-dependencies.txt for library/version
 # for each library, ask scruffy how many released versions away the version 
 # I have is from what I got.  
@@ -8,10 +8,17 @@
 import logging
 import re
 import optparse
+import requests
 
+from bs4 import BeautifulSoup
+import requests
+
+# http://stackoverflow.com/questions/11887762/how-to-compare-version-style-strings
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
 
 def parse_file2(log, filename):
-    """docstring for parse_file2"""
+    """"""
     with open(filename, 'r') as dep_file:
        for line in dep_file:
            if line:
@@ -22,7 +29,7 @@ def parse_file2(log, filename):
                                                                 match.group(3))
 
 def parse_file(log, filename):
-    """docstring for parse_file"""
+    """ tried to speed things up by using a compiled pattern, doesn't work :( """
     with open(filename, 'r') as dep_file:
        regex = re.compile("(?P<org>^([\w\d.]+)*?)#(?P<lib>([\w\d-]+)*?);(?P<version>([\d.-]+))$")
        for line in dep_file:
@@ -39,6 +46,19 @@ def parse_file(log, filename):
                else:
                    log.debug('no match found in [{0}]'.format(line))
 
+def get_versions_from_scruffy(log, module_url):
+    """docstring for get_versions_from_scruffy"""
+    url = 'http://scruffy.netflix.com/modules/%s'
+
+    r = requests.get(url % module_url)
+    soup = BeautifulSoup(r.text)
+    
+    release_versions = []
+    for revision in soup.find_all("a", class_="rev release"):
+        release_versions.append(revision.contents)
+
+    print release_versions
+
 def main():
     logging.basicConfig(filename='dep.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
     console = logging.StreamHandler()
@@ -52,8 +72,9 @@ def main():
     options, arguments = p.parse_args()
 
     parse_file2(log, options.file)
+
+    get_versions_from_scruffy(log, 'netflix/astyanax')
     log.info("finished")
 
 if __name__ == '__main__':
     main()
-
